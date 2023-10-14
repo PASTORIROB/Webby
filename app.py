8cf11ca5915cadb0f3bc0f7e63f330e645ec8b3a
Robert Pastori <PASTORIROB@GMAIL.COM>
2023-10-14 11:27:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "CV.pdf"
-profile_pic = current_dir / "assets" / "testrp.jpg"
+profile_pic = current_dir / "assets" / "newrp.png"
 #grad_pic = current_dir / "assets" / "grad.jpg"
 
 # --- GENERAL SETTINGS ---
